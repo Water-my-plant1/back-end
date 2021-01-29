6c21a07e09ca90bc2703b3afc8f7620a36f88f49
--global <--global>
2021-01-29 17:53:31 -0600

    Implemented cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const server = express();
 
 const usersMiddleware = require("./api/users/user.middleware");
@@ -8,6 +10,7 @@ const { plantsMiddleware } = require("./api/plants/plants.middleware");
 const plantsRouter = require("./api/plants/plants.model");
 
 server.use(express.json());
+server.use(cors());
 
 server.use("/api/users", usersMiddleware, userRouter);
 server.use("/api/plants", plantsMiddleware, plantsRouter);
