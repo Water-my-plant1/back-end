2e664303caa382440ceab445843b97da68edea4f
--global <--global>
2021-01-29 17:48:46 -0600

    Authenticated users can now create plants.

--- server/api/plants/plants.actions.js
+++ server/api/plants/plants.actions.js
@@ -3,16 +3,30 @@ const db = require("../../../data/dbconfig");
 const createPlant = async (req, res) => {
   try {
     const { nickname, species, h2ofrequency } = req.body;
+    const { id: user_id } = req.user;
 
     const result = await db("plants").insert({
       nickname,
       species,
       h2ofrequency,
+      user_id,
     });
 
-    return res.json({ result });
+    const createdPlant = {
+      id: result[0],
+      nickname,
+      species,
+      h2ofrequency,
+      user_id,
+    };
+
+    return res.json({
+      createdPlant,
+      message: `Plant created by ${req.user.username}`,
+    });
   } catch (error) {
     console.log(error);
+    return error;
   }
 };
 
